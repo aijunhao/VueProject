414e168a261e54d94eb742d076dcb12c3e45bfe9
aijunhao <1172949809@qq.com>
2019-04-11 21:09:50 +0800

    完成tabbar及路由的基本通路

--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 2. 制作底部的 `Tabbar` 区域，使用 `MUI` 的 `Tabbar` ，从 `Tabbar.html` 代码中拷贝。
 
     有四个按钮，分别是首页，会员，购物车，搜索。
-    
+
     - “首页”按钮图标使用默认。
     - “搜索”按钮图标修改。
     - “会员”和“购物车”按钮图标使用额外的图标。
@@ -18,3 +18,24 @@
 
 3. 在页面中间放置一个 `router-view` 来展示路由匹配到的组件。
 
+## 路由改造
+
+1. 改造 `tabbar` 为 `router-link`，并添加 `to` 属性。
+
+2. 设置路由高亮
+
+    在 `router.js` 文件中创建路由对象的时候添加自定义激活类，`linkActiveClass: 'mui-active'`，类名为 `MUI` 默认激活样式类。
+
+3. 创建路由组件
+
+    所有与 `tabbar` 有关的路由组件都放在 `components/tabbar` 目录下。
+
+    - HomeComponent.vue
+
+    - MemberComponent.vue
+
+    - SearchComponent.vue
+
+    - ShopcarComponent.vue
+
+4. 给 `router.js` 导入路由组件并挂载
--- src/router.js
+++ src/router.js
@@ -1,10 +1,30 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Home from './components/tabbar/HomeComponent.vue'
+import Member from './components/tabbar/MemberComponent.vue'
+import Search from './components/tabbar/SearchComponent.vue'
+import Shopcar from './components/tabbar/ShopcarComponent.vue'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
-
-  ]
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/member',
+      component: Member
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
+      path: '/shopcar',
+      component: Shopcar
+    }
+  ],
+  linkActiveClass: 'mui-active'
 })
